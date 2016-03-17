1e615fff754672dbbbb8f63c451713d1a1457484
Skyost <me@skyost.eu>
2016-03-17 09:31:50 +0100

    Premier commit : - Template du projet élève.

---
+++ src/fr/isn/bbq/eleve/ProjetBBQEleve.java
@@ -0,0 +1,20 @@
+package fr.isn.bbq.eleve;
+
+public class ProjetBBQEleve {
+	
+	/*
+	 * Variables globales du projet.
+	 */
+	
+	public static final String APP_NAME = "Projet BBQ";
+	public static final String APP_VERSION = "0.1";
+	
+	/**
+	 * Première méthode exécutée par le programme.
+	 * 
+	 * @param args Arguments à passer.
+	 */
+	
+	public static final void main(final String[] args) {}
+
+}
